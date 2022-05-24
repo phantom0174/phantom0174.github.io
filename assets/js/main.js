e49904c327d02de0ca9d57c133a945883eff098b
phantom0174 <darrenhsiou@gmail.com>
2022-05-24 08:36:01 +0800

    katex rendering

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+// console.log('haha!');
+
+
+// let to_render_list = document.getElementsByClassName("maf-eq");
+// to_render_list = Array.prototype.slice.call(to_render_list);
+
+// //console.log(to_render_list);
+
+// to_render_list.forEach(element => {
+//     console.log(element.innerHTML);
+
+//     const rendered_HTML = katex.renderToString(element.innerHTML, {
+//         throwOnError: false
+//     });
+//     element.parentNode.replaceChild(rendered_HTML, element);
+// });
