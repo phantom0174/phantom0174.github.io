55cbf47adac878c5ff953ce6b2c60da2d609ab72
Phantom0174 <darrenhsiou@gmail.com>
2023-02-15 21:56:22 +0800

    use fluid injector

--- scripts/disqus_recommend.js
+++ scripts/disqus_recommend.js
@@ -1,4 +1,15 @@
-const inject_code = `
+let inject_code = `
+<div id="disqus_recommendations"></div>
+`;
+
+hexo.extend.filter.register('theme_inject', function (injects) {
+    // weight of comments is 99 
+    // source: ./themes/fluid/scripts/filters/default-injects.js
+    injects.postComments.raw('disqus-reco', inject_code, '', '', -200);
+});
+
+
+inject_code = `
 <script> 
     (function() {
     var d = document, s = d.createElement('script');
