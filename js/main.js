ed4fe8869d7d163624d1aa12fbedb8a8f3686c76
Phantom0174 <darrenhsiou@gmail.com>
2023-02-24 00:00:49 +0800

    update post

--- js/main.js
+++ js/main.js
@@ -1,23 +1,19 @@
-const CNCONFIG = {
-    max_d: 100,
-    point_count: 70,
+new CanvasNice({
+    point_dist: 100,
+    point_count: 50,
+    point_size: {
+        min: 1,
+        max: 1.5
+    },
+    point_slow_down_rate: 0.8,
     point_color: '180,180,180',
     line_color: '180,180,180',
-    chunk_current_capacity: 15,
-    chunk_min_capacity: 5,
-    chunk_max_capacity: 20,
-    chunk_size_optimize_constant: 1, // recommended
-    particle_max_speed: 1, // pixel in each dimension per frame
-    particle_slow_down_rate: 0.8, // decrease function
-    gravity_constant: 1,
-    pointer_gravity_constant: 10,
-    lazy_overload_threshold: 1.1, // <= 1: active, >1: disabled
-    tabu_index: 0.5, // in seconds
-    GRAVITY_ACTIVE: false,
-    RANDOMNESS_ACTIVE: true,
+    line_width_multiplier: 1,
+    max_point_speed: 1,
     zIndex: -1,
     canvas_opacity: 1,
-    pointer_interaction: 0, // 0: points stop moving when touched by cursor; 1: same effect as canvas-nest.js
-};
-
-new CanvasNoice();
+    render_rate: 45,
+    chunk_capacity: 15,
+    chunk_size_constant: 0.8,
+    pointer_inter_type: 0
+});
