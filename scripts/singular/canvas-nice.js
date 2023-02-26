08d1203e00227b2bd52b42274af121605df29f24
Phantom0174 <darrenhsiou@gmail.com>
2023-02-26 23:00:56 +0800

    use cdn, update style

--- scripts/singular/canvas-nice.js
+++ scripts/singular/canvas-nice.js
@@ -1,6 +1,6 @@
 const code = `
-<script type="text/javascript" src="/js/canvas-nice.min.js"></script>
-<script type="text/javascript" src="/js/main.js"></script>
+<script defer type="text/javascript" src="https://cdn.jsdelivr.net/npm/canvas-nice.js/dist/canvas-nice.min.js"></script>
+<script defer type="text/javascript" src="/js/main.js"></script>
 `;
 
 hexo.extend.injector.register('body_end', code, 'home');
--- source/js/main.js
+++ source/js/main.js
@@ -1,19 +1,19 @@
 new CanvasNice({
-    point_dist: 100,
-    point_count: 50,
+    point_dist: 120,
+    point_count: 60,
     point_size: {
         min: 1,
-        max: 1.5
+        max: 2
     },
     point_slow_down_rate: 0.8,
-    point_color: '180,180,180',
-    line_color: '180,180,180',
-    line_width_multiplier: 1,
-    max_point_speed: 1,
+    point_color: '238,232,190',
+    line_color: '212,195,55',
+    line_width_multiplier: 1.5,
+    max_point_speed: 1.5,
     zIndex: -1,
     canvas_opacity: 1,
     render_rate: 45,
     chunk_capacity: 15,
     chunk_size_constant: 0.8,
-    pointer_inter_type: 0
+    pointer_inter_type: -1
 });
