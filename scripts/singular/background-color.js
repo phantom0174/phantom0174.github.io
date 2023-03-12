b4d7303092a64ca6ede1ce40b7579f312630ac08
Phantom0174 <darrenhsiou@gmail.com>
2023-03-12 22:35:36 +0800

    fix default-dark issues

---
+++ scripts/singular/background-color.js
@@ -0,0 +1,9 @@
+const disable_color_transition_animation = `
+<style>
+    body {
+        transition: none !important;
+    }
+</style>
+`;
+
+hexo.extend.injector.register('body_end', disable_color_transition_animation, 'default');
