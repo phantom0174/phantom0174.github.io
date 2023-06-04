5a17ff0a5ed9223a84122b695777f9ad797d506d
Phantom0174 <darrenhsiou@gmail.com>
2023-06-04 23:59:34 +0800

    new post: origin

--- scripts/pages/post.js
+++ scripts/pages/post.js
@@ -55,3 +55,27 @@ const code_fonts = `
 </style>
 `;
 hexo.extend.injector.register('head_begin', code_fonts, 'post');
+
+// source: https://tsuiokuyo.netlify.app/posts/5a26/
+const yt_tag = `
+<style>
+    .video-container {
+        position: relative;
+        padding-bottom: 56.25%;
+        padding-top: 30px;
+        margin-bottom: 15px;
+        height: 0;
+        overflow: hidden;
+    }
+
+    .video-container iframe, .video-container object, .video-container embed {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+    }
+</style>
+`;
+
+hexo.extend.injector.register('head_begin', yt_tag, 'post');
