3b90921e9afc20d9f8adf89ee95441c8bb9dba7f
Phantom0174 <darrenhsiou@gmail.com>
2024-10-28 21:25:56 +0800

    repos repo

---
+++ js/background-animation.js
@@ -0,0 +1,25 @@
+const device_width = window.innerWidth
+    || document.documentElement.clientWidth
+    || document.body.clientWidth;
+
+if (device_width > 600) {
+    new CanvasNice({
+        point_dist: 100,
+        point_count: 60,
+        point_size: {
+            min: 1,
+            max: 2
+        },
+        point_slow_down_rate: 0.8,
+        point_color: '238,232,190',
+        line_color: '212,195,55',
+        line_width_multiplier: 1.5,
+        max_point_speed: 1.5,
+        zIndex: -1,
+        canvas_opacity: 1,
+        render_rate: 30,
+        chunk_capacity: 15,
+        chunk_size_constant: 1,
+        pointer_inter_type: -1
+    });
+}
